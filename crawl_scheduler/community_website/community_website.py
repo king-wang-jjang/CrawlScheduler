b44246a6e541dd278fcac969d914d9f7b2238c6b
woody <woody@aicess.ai>
2025-01-04 00:12:35 +0900

    🚀 서버용 변수 설정

--- crawl_scheduler/community_website/community_website.py
+++ crawl_scheduler/community_website/community_website.py
@@ -34,7 +34,7 @@ class AbstractCommunityWebsite(ABC):  # ABC 클래스 상속 추가
     @abstractmethod
     def save_file(self, url, board_id, alt_text=None):
         child_class_name = self.__class__.__name__
-        path = os.path.join(Config().get_env('root'), child_class_name, str(board_id))
+        path = os.path.join(Config().get_env('ROOT'), child_class_name, str(board_id))
         logger.info(f"Saving image from URL: {url}")
         os.makedirs(path, exist_ok=True)
         if alt_text:
